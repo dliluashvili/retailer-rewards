8d88d94b967a6bf9cb4e3d72be61e816ee4f0d7e
Davit Liluashvili <david.liluashvili7@gmail.com>
2022-07-28 11:53:02 +0400

    added price field into create-payment dto

--- src/modules/payments/dtos/create-payment.dto.ts
+++ src/modules/payments/dtos/create-payment.dto.ts
@@ -2,6 +2,7 @@ import { IPayment } from '../payment.interface'
 
 export class CreatePaymentDto implements IPayment {
     user_id: number
+    price: number
     product: string
     description: string
 }
