7b1b513ce6a3b47b264edabe0cf980fd53271167
Davit Liluashvili <david.liluashvili7@gmail.com>
2022-07-29 23:05:19 +0400

    added find method in users controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 import { CreateUserDto } from './dtos/create-user.dto'
 import { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    find() {
+        return this.usersService.find()
+    }
 
     @Post()
     create(@Body() createUserDto: CreateUserDto) {
