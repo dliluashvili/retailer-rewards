58ae19cf9722c7615aceb6979f5a103a318682e5
Davit Liluashvili <david.liluashvili7@gmail.com>
2022-07-29 19:33:19 +0400

    imported connection service and clearDb method before seeding

--- src/database/seeders/seeder.ts
+++ src/database/seeders/seeder.ts
@@ -1,3 +1,4 @@
+import { Connection } from 'typeorm'
 import { MonthlyReportSeederService } from './monthly-report/monthly-report-seeder.service'
 import { PaymentsSeederService } from './payments/payments-seeder.service'
 import { Injectable } from '@nestjs/common'
@@ -8,12 +9,21 @@ export class Seeder {
     constructor(
         private readonly userSeederService: UserSeederService,
         private readonly paymentsSeederService: PaymentsSeederService,
-        private readonly monthlyReportSeederService: MonthlyReportSeederService
+        private readonly monthlyReportSeederService: MonthlyReportSeederService,
+        private readonly connection: Connection
     ) {}
 
     async run() {
+        await this.clearDB()
         await this.userSeederService.create()
         await this.paymentsSeederService.create()
         await this.monthlyReportSeederService.create()
+    }
+
+    private async clearDB() {
+        await this.connection.query('DELETE from users where id > 0;')
+        await this.connection.query(
+            'TRUNCATE TABLE users RESTART IDENTITY CASCADE;'
+        )
     }
 }
