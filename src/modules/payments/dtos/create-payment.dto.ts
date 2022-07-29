ae702ad7c64d9c3297a365b1965919612d06c9e4
Davit Liluashvili <david.liluashvili7@gmail.com>
2022-07-30 03:44:06 +0400

    added comment in create payment dto

--- src/modules/payments/dtos/create-payment.dto.ts
+++ src/modules/payments/dtos/create-payment.dto.ts
@@ -6,5 +6,6 @@ export class CreatePaymentDto implements IPayment {
     calculated_point?: number
     product: string
     description: string
+    // For seeder and development purpose
     created_at?: Date
 }
