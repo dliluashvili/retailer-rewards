9009dd3dcaf69ed5e64efb25eeb9da94f801d5d2
Davit Liluashvili <david.liluashvili7@gmail.com>
2022-07-30 17:59:01 +0400

    added sql schemas

---
+++ ddl/monthly-report.sql
@@ -0,0 +1,17 @@
+create table monthly_report
+(
+    id      serial
+        constraint "PK_fe8b38d177d9ee8af29e9d970e9"
+            primary key,
+    date    timestamp not null,
+    point   integer   not null,
+    user_id integer   not null
+        constraint "FK_bb688bbaf05065b39b1822c82c1"
+            references users
+            on delete cascade,
+    quarter integer   not null
+);
+
+alter table monthly_report
+    owner to postgres;
+
