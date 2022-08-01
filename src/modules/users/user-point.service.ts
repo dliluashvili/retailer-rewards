d280ec730cdfc76087e83915b831de728e650795
Davit Liluashvili <david.liluashvili7@gmail.com>
2022-08-01 21:16:27 +0400

    added check if price is less than 1

--- src/modules/users/user-point.service.ts
+++ src/modules/users/user-point.service.ts
@@ -1,9 +1,16 @@
+import { CustomBadRequestException } from './../../exceptions/custom-bad-request.exception'
 import { OVERDOLLARSPENT } from './../../constants/index'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class UserPointService {
     calculate(price: number): number {
+        if (price < 1) {
+            throw new CustomBadRequestException(
+                'An incorrect price is provided'
+            )
+        }
+
         const overDollars = Object.keys(OVERDOLLARSPENT).map((key) =>
             parseInt(key)
         )
